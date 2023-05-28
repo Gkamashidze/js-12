9b9df4306622ee57794187f2506ac4c9345d24c4
Gkamashidze <gqamashidze51@gmail.com>
2023-05-29 00:44:40 +0400

    განახლება

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
+// function check() {
+//     var ricxvi = document.getElementById("number").value;
+
+//     if (ricxvi < 0) {
+//         document.getElementById("dark").style.display = "flex";
+//         document.getElementById("message").innerHTML = "შეყვანილი რიცხვი არის უარყოფითი!"
+//     } else if (ricxvi > 0) {
+//         document.getElementById("dark").style.display = "flex";
+//         document.getElementById("message").innerHTML = "შეყვანილი რიცხვი არის დადებითი!"
+//     }
+// }
+
+// function closebtn() {
+//     document.getElementById("dark").style.display = "none";
+// }
+
+// function show() {
+
+//     var ricxvi = document.getElementById("number").value;
+
+//     if (ricxvi < 0) {
+//         document.getElementById("box1").style.height = "400px"
+//         document.getElementById("box1").style.backgroundColor = "red"
+//     } else if (ricxvi > 0) {
+//         document.getElementById("box1").style.height = "400px"
+//         document.getElementById("box1").style.backgroundColor = "green"
+//     } else if (ricxvi == 0) {
+//         document.getElementById("box1").style.height = "400px"
+//         document.getElementById("box1").style.backgroundColor = "grey"
+//     }
+
+// }
+
+
 function check() {
     var ricxvi = document.getElementById("number").value;
 
-    if (ricxvi < 0) {
-        document.getElementById("dark").style.display = "flex";
-        document.getElementById("message").innerHTML = "შეყვანილი რიცხვი არის უარყოფითი!"
-    } else if (ricxvi > 0) {
-        document.getElementById("dark").style.display = "flex";
-        document.getElementById("message").innerHTML = "შეყვანილი რიცხვი არის დადებითი!"
-    }
-}
+    document.write("hello world");
 
-function closebtn() {
-    document.getElementById("dark").style.display = "none";
-}
-
-function show() {
-
-    var ricxvi = document.getElementById("number").value;
-
-    if (ricxvi < 0) {
-        document.getElementById("box1").style.height = "400px"
-        document.getElementById("box1").style.backgroundColor = "red"
-    } else if (ricxvi > 0) {
-        document.getElementById("box1").style.height = "400px"
-        document.getElementById("box1").style.backgroundColor = "green"
-    } else if (ricxvi == 0) {
-        document.getElementById("box1").style.height = "400px"
-        document.getElementById("box1").style.backgroundColor = "grey"
-    }
-
+    document.getElementById("message").value="hello";
 }
